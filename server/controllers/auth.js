15c3931acdd809a9131f8629744ec49f2affcda2
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-28 12:57:14 +0300

    Auth & client rebuild

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,7 +20,7 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({ name: req.body.name });
+    const user = await User.findOne({ email: req.body.email });
     if (!user) return next(createError(404, "User not found !"));
 
     const isCorrect = await bcrypt.compare(req.body.password, user.password);
